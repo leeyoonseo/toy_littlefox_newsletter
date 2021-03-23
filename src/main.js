625dd94ef37e59f2f6787226e68bde3a5ff1ee60
leeyoonseo <okayoon.lee@gmail.com>
2021-03-23 20:48:31 +0900

    firebase 설정, travis 제거

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { router } from './routes/index.js'
 import { store } from './store/index.js'
 
+// import './assets/js/jquery-3.4.1.min';
 import './assets/css/reset.css';
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/toy_littlefox_newsletter/'
-        : '/'
+    publicPath: '/'
+//     publicPath: process.env.NODE_ENV === 'production'
+//         ? '/toy_littlefox_newsletter/'
+//         : '/'
 }
